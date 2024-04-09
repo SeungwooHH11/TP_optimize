2621d54e0b07bebb2987bc204d83196096900c44
SeoungwooHan <150769291+SeungwooHH11@users.noreply.github.com>
2024-04-09 14:41:05 +0900

    Update Network_RPS.py

--- V2/Network_RPS.py
+++ V2/Network_RPS.py
@@ -318,6 +318,6 @@ class PPO(nn.Module):
                 'model_state_dict': self.state_dict(),
                 'optimizer_state_dict': self.optimizer.state_dict(),
 
-            }, model_dir+'trained_model' + str(step) + '.pth')
+            }, model_dir+'trained_model' + str(step1) + '.pth')
 
         return ave_loss, v_loss, p_loss
