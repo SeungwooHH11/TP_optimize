d9d8a2584ee2808f89bf367fe565909c396d5149
SeoungwooHan <150769291+SeungwooHH11@users.noreply.github.com>
2024-04-15 12:26:32 +0900

    Update main.py

--- EXP_1/main.py
+++ EXP_1/main.py
@@ -41,7 +41,7 @@ if __name__=="__main__":
     number_of_validation_batch=50
     number_of_problem=10 # 한번에 몇개의 문제를
     number_of_batch=80 # 문제당 몇 episode씩 한번에 학습할껀지
-    number_of_trial=1000  #1, 10, 100, 1000 #이를 몇번 반복할껀지
+    number_of_trial=1  #1, 10, 100, 1000 #이를 몇번 반복할껀지
     number_of_iteration=int(5000/number_of_trial)  # 전체 iteration #iteration 단위로 문제 변화
     validation=[]
     validation_step = 10
@@ -109,8 +109,8 @@ if __name__=="__main__":
                     Control_result[temp_step, nu, 5] = ts.var()
                 temp_step+=1
 
-            for nu,mod in enumerate(mode_list):
-                print(mod,Control_result[0:temp_step,nu,0].mean(),Control_result[0:temp_step,nu,2].mean(),Control_result[0:temp_step,nu,4].mean())
+                for nu,mod in enumerate(mode_list):
+                    print(mod,Control_result[0:temp_step,nu,0].mean(),Control_result[0:temp_step,nu,2].mean(),Control_result[0:temp_step,nu,4].mean())
 
         for k in range(number_of_trial):
             ave_reward = 0
