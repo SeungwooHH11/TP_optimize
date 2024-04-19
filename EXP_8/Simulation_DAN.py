1880abacaa93c3798258bbff0879f855c0260030
SeoungwooHan <150769291+SeungwooHH11@users.noreply.github.com>
2024-04-19 18:22:44 +0900

    Update Simulation_DAN.py

--- EXP_8/Simulation_DAN.py
+++ EXP_8/Simulation_DAN.py
@@ -145,10 +145,10 @@ def simulation(B, T, transporter, block, edge_fea_idx, node_fea, edge_fea, dis, 
                 pri[0][i]=max(dis[int(start_location)][n]/120/tardy_high,edge_fea[n,e,1].item())+edge_fea[n,e,0].item()
                 pri[1][i]=dis[int(start_location)][n] / 120 / tardy_high
                 #pri[2][i]=edge_fea[n,e,1].item()
-                #pri[2][i]=-(1/edge_fea[n,e,0]*torch.exp(-(edge_fea[n,e,2])/(torch.sum(edge_fea[:,:,0])/valid_coords.shape[0]))).item()
-                pri[2][i]=edge_fea[n,e,2].item()
-                #pri[4][i]=-(1/edge_fea[n,e,0]*(1-(edge_fea[n,e,2]/edge_fea[n,e,0]))).item()
-            for i in range(3):
+                pri[2][i]=-(1/edge_fea[n,e,0]*torch.exp(-(edge_fea[n,e,2])/(torch.sum(edge_fea[:,:,0])/valid_coords.shape[0]))).item()
+                pri[3][i]=edge_fea[n,e,2].item()
+                pri[4][i]=-(1/edge_fea[n,e,0]*(1-(edge_fea[n,e,2]/edge_fea[n,e,0]))).item()
+            for i in range(5):
                 value=np.unique(pri[i])
                 value1=value[0]
                 for j in np.where(value1==pri[i])[0]:
