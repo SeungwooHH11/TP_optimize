d9785b01a83decbec2b69ffe6df8174959d28e40
SeoungwooHan <150769291+SeungwooHH11@users.noreply.github.com>
2024-05-29 00:15:21 +0900

    Update main.py

--- EXP_5/main.py
+++ EXP_5/main.py
@@ -20,10 +20,10 @@ if __name__=="__main__":
         os.makedirs(history_dir)
 
     device='cuda'
-    block_number=50
+    block_number=40
     location_number=30
     transporter_type=2
-    transporter_number=10
+    transporter_number=8
     dis_high=3000
     dis_low=500
     ready_high=100 
