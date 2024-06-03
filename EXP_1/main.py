50960e85940f42e3fd92acb08df7ceecd6bc66cf
SeoungwooHan <150769291+SeungwooHH11@users.noreply.github.com>
2024-06-03 15:57:42 +0900

    Update main.py

--- EXP_1/main.py
+++ EXP_1/main.py
@@ -6,8 +6,12 @@ import torch
 import vessl
 np.random.seed(1)
 random.seed(1)
-torch.cuda.manual_seed(1)
-
+torch.manual_seed(1)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1)
+    torch.cuda.manual_seed_all(1)  # if you are using multi-GPU.
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
 
 if __name__=="__main__":
     problem_dir='/output/problem_set/'
