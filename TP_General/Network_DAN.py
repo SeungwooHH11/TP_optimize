7836a1a6963a787e985f9588fb9a6296be92f3bf
SeoungwooHan <150769291+SeungwooHH11@users.noreply.github.com>
2024-09-09 14:27:02 +0900

    Update Network_DAN.py

--- TP_General/Network_DAN.py
+++ TP_General/Network_DAN.py
@@ -156,8 +156,8 @@ class PPO(nn.Module):
         self.node_fea_len = 32
         self.final_node_len=32
         self.edge_fea_len = 32
-        self.gnn = CrystalGraphConvNet(orig_node_fea_len=int(2*self.transporter_type), orig_edge_fea_len=(3+self.transporter_type), edge_fea_len=self.edge_fea_len, node_fea_len=self.node_fea_len, final_node_len=32, dis=dis)
-        self.pi = MLP(32 + 10 + 5 + 5, 1).to(device)
+        self.gnn = CrystalGraphConvNet(orig_node_fea_len=int(2*self.transporter_type), orig_edge_fea_len=int(3+self.transporter_type), edge_fea_len=self.edge_fea_len, node_fea_len=self.node_fea_len, final_node_len=32, dis=dis)
+        self.pi = MLP(32 + 2*int(3+self.transporter_type) + 5 + 5, 1).to(device)
         self.temperature = 1
         self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
         self.lmbda = lmbda
