7d822afe95830d060cc5aff9a5ba30533d7057de
SeoungwooHan <150769291+SeungwooHH11@users.noreply.github.com>
2024-10-10 12:25:19 +0900

    Update Simulation_DAN.py

--- TP_Practical/Simulation_DAN.py
+++ TP_Practical/Simulation_DAN.py
@@ -31,7 +31,7 @@ class Problem_sampling:
             v = np.random.choice(self.Location_Number, 2, False)
             Block[i, 0], Block[i, 1] = v[0], v[1]
             Block[i, 2] = self.Dis[int(Block[i, 0]), int(Block[i, 1])] / 80 / self.tardy_high   #processing time
-            Block[i, 3] = 0
+            Block[i, 3] = np.random.randint(0, self.ready_high) / self.tardy_high   # ready time
             Block[i, 4] = 1
 
             weight = np.random.uniform(0, 50*self.Transporter_type)
