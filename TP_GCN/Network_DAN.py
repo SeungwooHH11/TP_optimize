3447795d07af6242cd4a9310022df6d1dde64973
SeoungwooHan <150769291+SeungwooHH11@users.noreply.github.com>
2025-04-05 14:02:07 +0900

    Update Network_DAN.py

--- TP_GCN/Network_DAN.py
+++ TP_GCN/Network_DAN.py
@@ -456,10 +456,10 @@ class PPO(nn.Module):
         for episode in data:
             advantage = 0.0
             
-            for t in reversed(range(i, i + len(episode))):
+            for t in reversed(range(i, i + len(episode)-1)):
                 advantage = self.gamma * self.lmbda * advantage + delta[t][0]
                 advantage_lst[t][0] = advantage
-            i += len(episode)
+            i += len(episode)-1
         ratio = torch.exp(torch.log(pi_a_total.unsqueeze(1)) - torch.log(probs))  # a/b == exp(log(a)-log(b))
 
         surr1 = ratio * advantage_lst
